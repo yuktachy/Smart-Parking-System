6a5898ec47307751df433e39b1b9b2b09d75499a
Somdyut Saha <somdyutisaha7@gmail.com>
2026-04-25 22:17:37 +0530

    Added OOP comments to code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,13 @@
 #include "classes.h"
 
 int main() {
+
+    // Encapsulation + Composition:
+    // ParkingManager object controls the entire system
+    // Internally it uses slots, file handler, etc.
     ParkingManager manager;
     int choice;
-
+  // Loop to continuously run the system (menu-driven program)
     while(true) {
         cout << "\n--- SMART PARKING SYSTEM ---\n";
         cout << "1. Park Vehicle\n";
@@ -14,21 +18,30 @@ int main() {
         cout << "6. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
-
+       // Control flow using switch-case
         switch(choice) {
             case 1:
+             // Abstraction:
+                // User does not know how parking is handled internally
+                // ParkingManager manages allocation, fee, QR, etc.
                 manager.parkVehicle();
                 break;
 
             case 2:
+              // Encapsulation:
+                // Exit logic handled inside ParkingManager
                 manager.removeVehicle();
                 break;
 
             case 3:
+            // SRP:
+                // Slot display is delegated to SlotDisplay class internally
                 manager.showSlots();
                 break;
 
             case 4: {
+                // Abstraction:
+                // FileHandler hides file reading implementation
                 FileHandler f;
                 f.readData();
                 break;
